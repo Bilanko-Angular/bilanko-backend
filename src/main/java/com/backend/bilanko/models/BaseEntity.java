ac446e735e964915f662dd50e5a5d5d7d9932998
EmmanuelSeujip <emmanuelseujip@gmail.com>
2026-09-03 12:29:30 +0100

    save

--- src/main/java/com/backend/bilanko/models/BaseEntity.java
+++ src/main/java/com/backend/bilanko/models/BaseEntity.java
@@ -1,4 +1,27 @@
 package com.backend.bilanko.models;
 
-public class BaseEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Setter
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
 }
